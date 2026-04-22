d3bb8c575043c8de7019ca840a64ada9eaeefe61
u24698149-hub <u24698149@tuks.co.za>
2026-04-23 00:48:33 +0200

    Add router root file

---
+++ src/router/__root.tsx
@@ -0,0 +1,10 @@
+import { createRootRoute } from '@tanstack/react-router'
+import { Outlet } from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+  component: () => (
+    <div>
+      <Outlet />
+    </div>
+  ),
+})
